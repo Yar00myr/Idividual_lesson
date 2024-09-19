e2207020706adb561ea74cc91a08df517ff2bdf8
Yar00myr <ivanucaromir@gmail.com>
2024-09-20 00:21:09 +0300

    I continue to make adjustments

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,4 +1,5 @@
-from flask import render_template
+import random
+from flask import render_template, redirect
 from .. import app
 from ..helpers import students_data
 
@@ -8,3 +9,14 @@ def index():
     json_path = 'students_mark.json'
     students_mark = students_data(json_path)
     return render_template("students.html", students_mark_=students_mark)
+
+
+@app.route("/information")
+def info():
+    return render_template("info.html")
+
+
+@app.route("/random-endpoint")
+def random_endpoint():
+    endpoint = random.choice(["/", "/information"])
+    return redirect(endpoint)
